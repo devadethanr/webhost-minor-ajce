bc7d837625c9d2975e328576cfbbbec5bd8a0bb1
devadethanr <125908755+devadethanr@users.noreply.github.com>
2023-11-01 16:03:05 +0530

    commit

---
+++ dethan-react/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Syntheia <code>Welcomes you. </code> Syntheia- new app just created.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from Syntheia.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ dethan-react/my-app/src/Dethan.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Dethan() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          New file testing from dethan
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from Syntheia.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Dethan;
